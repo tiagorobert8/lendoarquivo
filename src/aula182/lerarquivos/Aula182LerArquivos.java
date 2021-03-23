13785d33a952c8e2aebbffee0cfdbe611f0c1bd5
tiagorobert8 <tiagorobert8@gmail.com>
2021-03-23 15:41:35 -0300

    Ler arquivo txt

---
+++ src/aula182/lerarquivos/Aula182LerArquivos.java
@@ -0,0 +1,33 @@
+package aula182.lerarquivos;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Aula182LerArquivos {
+
+    
+           public static void main(String[] args) {
+               
+                    File file = new File("C:\\temp\\in.txt");
+                    Scanner sc = null;
+               try {
+                      sc = new  Scanner(file);
+                      while  (sc.hasNextLine()){
+                               System.out.println(sc.nextLine());
+                               
+                                }
+                     }
+                  catch (IOException e){
+                          System.out.println("Error:  " + e.getMessage());
+                             
+                     }
+                     finally{
+                                if (sc  != null ) {
+                                    sc.close();
+                                }
+                    }
+           }
+    
+}
